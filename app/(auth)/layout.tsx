c28cc5f183d5c03727fcc7dcd17f954197d0e3e4
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-19 16:08:12 +0700

    create the Auth group file

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+const AuthLayout = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	return( 
+		<div className="flex items-center h-full justify-center">
+			{children}
+		</div>
+	);
+};
+export default AuthLayout;
